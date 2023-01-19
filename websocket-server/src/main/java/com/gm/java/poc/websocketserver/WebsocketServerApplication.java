0e3a440e4ab742778b33f68a6cec06048ac37dcd
gishnum <42067062+gishnum@users.noreply.github.com>
2023-01-19 22:08:49 +0530

    initail commit

---
+++ websocket-server/src/main/java/com/gm/java/poc/websocketserver/WebsocketServerApplication.java
@@ -0,0 +1,13 @@
+package com.gm.java.poc.websocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketServerApplication.class, args);
+	}
+
+}
---
+++ websocket-server/src/test/java/com/gm/java/poc/websocketserver/WebsocketServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm.java.poc.websocketserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
